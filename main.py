785a830e23da5b41a32672f8cb668a7162caf167
MahakkapanPlangklang <119285823+MahakkapanPlangklang@users.noreply.github.com>
2025-04-03 23:13:11 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI, HTTPException
+from pymongo import MongoClient
+from fastapi.middleware.cors import CORSMiddleware
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+MONGO_URI = os.getenv("MONGO_URI")
+client = MongoClient(MONGO_URI)
+db = client["MobileLegend_wiki_backend"]
+collection = db["characterinfos"]
+
+def fix_id(doc):
+    doc["_id"] = str(doc["_id"])
+    return doc
+
+@app.get("/heroes")
+def get_all_heroes():
+    heroes = list(collection.find())
+    return [fix_id(hero) for hero in heroes]
+
+@app.get("/heroes/name/{name}")
+def get_hero_by_name(name: str):
+    hero = collection.find_one({"HeroName": name})
+    if not hero:
+        raise HTTPException(status_code=404, detail="Hero not found")
+    return fix_id(hero)
+
+@app.get("/heroes/type/{type_name}")
+def get_heroes_by_type(type_name: str):
+    heroes = list(collection.find({"Role": type_name}))
+    if not heroes:
+        raise HTTPException(status_code=404, detail=f"No heroes found in type: {type_name}")
+    return [fix_id(hero) for hero in heroes]
